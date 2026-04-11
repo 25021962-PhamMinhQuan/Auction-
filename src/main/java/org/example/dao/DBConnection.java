71cd6d03d0d6b7084f92a62d6c81f98db07f6779
25021962-PhamMinhQuan <25021962@vnu.edu.vn>
2026-04-11 21:21:03 +0700

    Chỉnh sử các truy cập dataBase. không truy cập trực tiếp nữa mà giờ là qua config

--- src/main/java/org/example/dao/DBConnection.java
+++ src/main/java/org/example/dao/DBConnection.java
@@ -3,9 +3,31 @@ package org.example.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.io.*;
+import java.util.Properties;
 
 public class DBConnection {
+    private static String url;
+    private static String username;
+    private static String password;
+
+    static{
+        try{
+            Properties props = new Properties();
+            InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("config.properties");
+            if(input == null){
+                throw new RuntimeException("config not found");
+            }
+            props.load(input);
+            url = props.getProperty("db.url");
+            username = props.getProperty("db.username");
+            password = props.getProperty("db.password");
+
+        } catch (IOException e) {
+            throw new RuntimeException("can't read config", e);
+        }
+    }
     public static Connection getConnection() throws SQLException{
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/AUCTION_SYSTEM","root","13489103");
+        return DriverManager.getConnection(url,username,password);
     }
 }
