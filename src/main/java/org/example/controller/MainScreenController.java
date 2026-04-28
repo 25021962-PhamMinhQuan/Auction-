32900007dab1b852db6d399bdd5a2ebab99399dd
Viet Quang <25021955@vnu.edu.vn>
2026-04-28 12:49:49 +0700

    Tách Controller

--- src/main/java/org/example/controller/MainScreenController.java
+++
@@ -1,9 +0,0 @@
-package org.example.controller;
-import javafx.fxml.FXML;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-
-
-public class MainScreenController {
-}
